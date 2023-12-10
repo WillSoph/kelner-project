ff727479cfac032789f3bf7651fc899f1b58ccfe
Willians Ferreira <williansdesigner@hotmail.com>
2023-12-10 10:41:06 -0300

    removing prettier

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,11 @@
 import Stripe from 'stripe'
 
+const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
+
+if (!stripePublicKey) {
+  throw new Error('Chave pública do Stripe não definida');
+}
+
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: '2020-08-27',
   appInfo: {
