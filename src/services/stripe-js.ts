c064a5e64475a720f3798691f041ef854f33c1c2
Willians Ferreira <williansdesigner@hotmail.com>
2023-12-10 11:04:02 -0300

    removing prettier

--- src/services/stripe-js.ts
+++ src/services/stripe-js.ts
@@ -1,5 +1,11 @@
 import { loadStripe } from '@stripe/stripe-js'
 
+const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY;
+
+if (!stripePublicKey) {
+  throw new Error('Chave pública do Stripe não definida');
+}
+
 export async function getStripeJs() {
   const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY)
 
